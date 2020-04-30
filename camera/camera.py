a5454aec04cab4db74a2e1810621d16f74e7a4c1
Agenicy <agenicy@gmail.com>
2020-05-01 00:05:37 +0800

    開始抓板子色塊

--- camera/camera.py
+++ camera/camera.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
-
+  
 
 class camera():
-    def __init__(self, url='http://192.168.137.249:4747/mjpegfeed'):
+    def __init__(self, url):
         super().__init__()
         # 開啟網路攝影機
         self.cam = cv2.VideoCapture(url)
@@ -14,11 +14,14 @@ class camera():
         self.w = 480
         self.h = 640
         self.border = 50  # resize border px
+        self.M = None
+        self.color_sensitive = 0
 
         self.point = []
         for i in range(1, 10):
             for j in range(1, 10):
                 self.point.append((i*self.border, j*self.border))
+                
 
     # 定义旋转rotate函数
     def rotate(self, img, angle, center=None, scale=1.0):
@@ -48,6 +51,7 @@ class camera():
         #! camera 區域
         img = img[:, 80:-80]
         img = cv2.resize(img, (self.pict_size, self.pict_size))
+        img[:] = 0.9 * img[:]
         return img
 
     def detect_border(self, blur_gray):
@@ -59,58 +63,103 @@ class camera():
 
     def resize(self, img):
         # 將圖片轉為灰階
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                blur_gray = cv2.GaussianBlur(
-                    gray, (self.kernel_size, self.kernel_size), 0)
-                try:
-                    edges = self.detect_border(blur_gray)
-                except:
-                    pass
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur_gray = cv2.GaussianBlur(
+            gray, (self.kernel_size, self.kernel_size), 0)
+            
+        try:
+            edges = self.detect_border(blur_gray)
+        except:
+            pass
 
-                contours, hierarchy = cv2.findContours(
-                    edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        
+        if len(contours) >=1:
+            
+            # find the biggest countour (c) by the area
+            self.board_area = max(contours, key=cv2.contourArea)
+            corner = cv2.approxPolyDP(
+                self.board_area, 0.1*cv2.arcLength(self.board_area, True), True)
 
-                if len(contours) != 0:
-                    try:
-                        # draw in blue the contours that were founded
-                        #cv2.drawContours(img, contours, -1, 255, 3)
+            corner = np.float32(corner.tolist())
+            
+            to = np.float32([[self.border, self.border], [self.border, self.pict_size-self.border], [
+                            self.pict_size-self.border, self.pict_size-self.border], [self.pict_size-self.border, self.border]])
 
-                        # find the biggest countour (c) by the area
-                        self.board_area = max(contours, key=cv2.contourArea)
-                        corner = cv2.approxPolyDP(
-                            self.board_area, 0.1*cv2.arcLength(self.board_area, True), True)
+            if len(corner) == 4:
+                # 取得變形公式
+                self.M = cv2.getPerspectiveTransform(corner, to)
+    
+    def get_board(self, img):
+        blur_gray = cv2.GaussianBlur(
+            img, (self.kernel_size, self.kernel_size), 0)
+        contours, hierarchy = cv2.findContours(blur_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        
+        if len(contours) >=1:
+            
+            # find the biggest countour (c) by the area
+            self.board_area = max(contours, key=cv2.contourArea)
+            corner = cv2.approxPolyDP(
+                self.board_area, 0.1*cv2.arcLength(self.board_area, True), True)
 
-                        corner = np.float32(corner.tolist())
-                        to = np.float32([[self.border, self.border], [self.border, self.pict_size-self.border], [
-                                        self.pict_size-self.border, self.pict_size-self.border], [self.pict_size-self.border, self.border]])
+            corner = np.float32(corner.tolist())
+            
+            to = np.float32([[self.border, self.border], [self.border, self.pict_size-self.border], [
+                            self.pict_size-self.border, self.pict_size-self.border], [self.pict_size-self.border, self.border]])
 
-                        # 取得變形公式
-                        self.M = cv2.getPerspectiveTransform(corner, to)
-                        self.resize_mode = False
-                    except:
-                        pass
-                    
+            if len(corner) == 4:
+                # 取得變形公式
+                self.M = cv2.getPerspectiveTransform(corner, to)
+               
     def run(self):
+        
+        least, most = [], []
+        for i in range(20):
+            img = self.getImg()
+            while self.M is None:
+                self.resize(img)
+            
+            dst = cv2.warpPerspective(img, self.M, (self.pict_size, self.pict_size))
+            cv2.imshow('blurdst', dst)
+            blur = cv2.blur(dst, (64, 64))
+            least.append(min(blur[0].tolist()))
+            most.append(max(blur[0].tolist()))
+            
+            cv2.imshow('blur', blur)
+            
+        least = np.array(min(least))
+        most = np.array(max(most))
+        
+        least[:] = least[:] - self.color_sensitive
+        most[:] = most[:] + self.color_sensitive
+            
         while True:
             img = self.getImg()
+            mask = cv2.inRange(img, least, most)
+            #mask[:] = 255 - mask[:]
             
-            self.resize(img)
-            
-            # 透視變形
-            dst = cv2.warpPerspective(
-                img, self.M, (self.pict_size, self.pict_size))
+            try:
+                self.get_board(mask)
+                # 透視變形
+                dst = cv2.warpPerspective(mask, self.M, (self.pict_size, self.pict_size))
+                
+                """
+                # draw points
+                for point in [(3*self.border,3*self.border),(7*self.border,7*self.border),(3*self.border,7*self.border),(7*self.border,3*self.border),(5*self.border,5*self.border)]:
+                    cv2.circle(dst, point, 8, (0, 255, 255), thickness=-1)
+                for point in self.point:
+                    cv2.circle(dst, point, 5, (0, 0, 255), thickness=-1)
+                """
 
-            # draw points
-            for point in [(3*self.border,3*self.border),(7*self.border,7*self.border),(3*self.border,7*self.border),(7*self.border,3*self.border),(5*self.border,5*self.border)]:
-                cv2.circle(dst, point, 8, (0, 255, 255), thickness=-1)
-            for point in self.point:
-                cv2.circle(dst, point, 5, (0, 0, 255), thickness=-1)
-            
-
-            cv2.drawContours(img, self.board_area, -1, (255, 0, 0), 3)
-            cv2.imshow('result', img)
-            cv2.imshow('resize', dst)
-            
+                cv2.drawContours(img, self.board_area, -1, (255, 0, 0), 3)
+                
+                cv2.imshow('result', img)
+                cv2.imshow('resize', dst)
+                cv2.imshow('mask', mask)
+                
+            except Exception as e:
+                print(e)
+        
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -118,16 +167,16 @@ class camera():
         img = self.getImg()
             
         self.resize(img)
-        
-        if self.M is not None:
+        try:
             # 透視變形
-            dst = cv2.warpPerspective(
-                img, self.M, (self.pict_size, self.pict_size))
+            dst = cv2.warpPerspective(img, self.M, (self.pict_size, self.pict_size))
             
             cv2.drawContours(img, self.board_area, -1, (255, 0, 0), 3)
-        return img, dst
+            return img, dst
+        except:
+            pass
 
 if __name__ == "__main__":
-    cam = camera()
+    cam = camera(url='http://127.0.0.1:4747/mjpegfeed')
     cam.run()
     cam.close()
--- camera/detect.py
+++ camera/detect.py
@@ -1,6 +1,6 @@
 
 class detect():
-    def __init__(self, wpx=500, hpx=500, points=13):
+    def __init__(self, wpx=500, hpx=500, points=9):
         super().__init__()
         self.w = wpx
         self.h = hpx
@@ -9,7 +9,7 @@ class detect():
     def analyze(self, dst):
         """取得當前棋盤所有落子位置"""
         while True:
-            dotList = self.getDot(img)
+            dotList = self.getDot(dst)
             dotChange, changed = self.getChange(dotList)
             if changed:
                 return self.dotListToString(dotChange)
@@ -18,8 +18,8 @@ class detect():
 if __name__ == "__main__":
     from camera import camera
     import cv2
-    cam = camera()
-    img, dst = cam.getDst()
-    cv2.imshow('result', dst)
+    cam = camera(url='http://127.0.0.1:4747/mjpegfeed')
+    while True:
+        cam.run()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
